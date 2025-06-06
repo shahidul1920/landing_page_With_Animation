3d61730975915bca3aeeddc5c1de12d31e76f977
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-06-07 00:36:57 +0600

    donno

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,29 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='headerSection py-4 px-2 sticky'>
+    <div className='headerSection p-4'>
       <div className="container mx-auto flex justify-between">
         <div className="logo">
             <img className='max-w-[120px]' src="https://d13pvy8xd75yde.cloudfront.net/global/x_new/logo.svg" alt="" />
         </div>
-        <div className="menu">
+        <div className="menu hidden md:flex">
             <ul className='flex gap-4 items-center'>
                 <li>Phone</li>
                 <li>Laptop</li>
                 <li>Accessories</li>
             </ul>
         </div>
-        <div className="info">
+        <div className="info hidden md:flex">
             <ul className='flex gap-4 items-center'>
                 <li>Community</li>
                 <li>Buy Now</li>
                 <li>Login</li>
             </ul>
+        </div>
+        <div className="toggleMenu md:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
         </div>
       </div>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className='w-[100%] h-[1140px] relative'>
-        <img src="https://d13pvy8xd75yde.cloudfront.net/global/tablets/megabook-k16s-amd/sec1_bg_pc.png" className='absolute inset-0 z-10' alt="" />
-      <div className="container relative z-20 mx-auto size-full pt-[18rem] gap-4">
-        <div className="models flex gap-1">
-            <img src="https://d13pvy8xd75yde.cloudfront.net/global/tablets/megabook-k16s-amd/sec1_tit_pc.png" alt="" />
-            <img className='h-[70px]' src="https://d13pvy8xd75yde.cloudfront.net/global/tablets/megabook-k16s-amd/sec1_tit_amd.png" alt="" />
-        </div>
-        <div className="titles">
-            <h1 className='text-6xl font-black'>See Bigger, Sound louder.</h1>
-            <h3 className='text-3xl font-medium'> 16:10 Wide Screen 300% Speaker Louder Mega Performance </h3>
+    <div className="w-[100%] h-screen relative">
+      <img
+        src="https://d13pvy8xd75yde.cloudfront.net/global/tablets/megabook-k16s-amd/sec1_bg_pc.png"
+        className="absolute inset-0 z-10"
+        alt=""
+      />
+      <div className="container relative z-20 mx-auto size-full">
+        <div className="size-full gap-4 flex flex-col justify-center">
+          <div className="models flex gap-2">
+            <img
+              className="w-[150px] md:w-[230px] xl:w-auto"
+              src="https://d13pvy8xd75yde.cloudfront.net/global/tablets/megabook-k16s-amd/sec1_tit_pc.png"
+              alt=""
+            />
+            <img
+              className="max-h-[20px] md:max-h-[40px] xl:max-h-[70px]"
+              src="https://d13pvy8xd75yde.cloudfront.net/global/tablets/megabook-k16s-amd/sec1_tit_amd.png"
+              alt=""
+            />
+          </div>
+          <div className="titles">
+            <h1 className="text-2xl md:text-4xl xl:text-6xl font-black">
+              See Bigger, Sound louder.
+            </h1>
+            <h3 className="md:text-2xl xl:text-3xl font-medium">
+              {" "}
+              16:10 Wide Screen 300% Speaker Louder Mega Performance{" "}
+            </h3>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
