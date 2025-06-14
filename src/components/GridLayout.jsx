1dbe64de41a4f5bc9dd4ec7ceaaf64612be8e623
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-06-14 19:54:21 +0600

    section added

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -3,8 +3,30 @@ import React from "react";
 const GridLayout = () => {
   return (
     <section className="px-4 xl:px-0 py-12 bg-[#DCDFE8]">
-      <div className="container mx-auto">
-        <div className="left"></div>
+      <div className="container mx-auto flex gap-6 flex-wrap">
+        <div className="left">
+          <div className="top flex gap-6 mb-6">
+            <div className="top-right max-w-[440px]">
+              <div className="topSub  p-6 bg-white rounded-xl">
+                <h2 className="text-xl md:text-2xl font-bold mb-4">
+                  16inch Big Vision <br />
+                  16:10 Exclusive Display
+                </h2>
+                <img className="w-full h-auto" src="https://d13pvy8xd75yde.cloudfront.net/global/ru/megabook/k16/sec2_1.png" alt="" />
+              </div>
+              <div className="botSub p-6 bg-[#E8E8E8] mt-6 rounded-xl">
+                <h2 className="text-xl md:text-2xl font-bold mb-4">
+                  180° Angle of view
+                </h2>
+                <img className="w-full h-auto" src="https://d13pvy8xd75yde.cloudfront.net/global/ru/megabook/k16/sec2_2.png" alt="" />
+              </div>
+            </div>
+            <div className="mid min-w-[full] xl:min-w-[560px] bg-amber-300">
+helo
+            </div>
+          </div>
+          <div className="bottom-top"></div>
+        </div>
         <div className="right max-w-[460px] overflow-hidden">
           <div className="item item1 w-full p-6 bg-white rounded-xl">
             <div className="title mb-4">
@@ -46,18 +68,35 @@ const GridLayout = () => {
 
           <div className="item item2 w-full p-6 bg-white mt-6 grid grid-cols-2 items-center rounded-xl">
             <div className="title">
-              <h3 className="text-3xl font-bold">Expandable Memory</h3>
-              <p className="text-[18px]">Up to 32GB</p>
+              <h3 className="text-xl md:text-3xl font-bold">
+                Expandable Memory
+              </h3>
+              <p className="md:text-[18px]">Up to 32GB</p>
             </div>
-            <div className="imag overflow-hidden">
-              <img className="h-[120px] object-cover"
+            <div className="imag overflow-hidden h-full bg-transparent">
+              <img
+                className="w-auto scale-150 img1"
                 src="https://d13pvy8xd75yde.cloudfront.net/global/ru/megabook/k16/sec2_11.png"
                 alt=""
               />
             </div>
           </div>
-          <div className="item item3 w-full p-6 bg-white shadow-2xl mt-6">
-
+          <div className="item item3 w-full p-6 bg-white shadow-2xl mt-6 rounded-xl">
+            <div className="title flex justify-between items-center mb-4">
+              <h3 className="text-xl md:text-3xl font-bold">
+                TECNO PC Manager
+              </h3>
+              <p className="md:text-[16px]">
+                OneLeap Collaboration PC Clone Optimization
+              </p>
+            </div>
+            <div className="imag overflow-hidden md:h-[240px] bg-transparent ">
+              <img
+                className="w-auto img2 transform translate-0 md:translate-x-0  md:-translate-y-[95px]"
+                src="https://d13pvy8xd75yde.cloudfront.net/global/ru/megabook/k16/sec2_12.png"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
