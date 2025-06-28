a46680ba3917d4176a21c5c7e19074e893180947
Shahidul Shakil <shahidul1920shakil@gmail.com>
2025-06-28 17:22:23 +0600

    new section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import GridLayout from '../components/GridLayout'
 import BuildSection from '../components/BuildSection'
 import Specs1 from '../components/Specs1'
 import Specs2 from '../components/Specs2'
+import Specs3 from '../components/Specs3'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <BuildSection />
       <Specs1 />
       <Specs2 />
+      <Specs3 />
     </div>
   )
 }
---
+++ src/components/Specs3.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Specs3 = () => {
+  return (
+    <div className='bg-black'>
+      <div className="container grid grid-cols-1 xl:grid-cols-2 items-center mx-auto">
+        <div className="text text-white">
+            <h2 className='text-6xl font-bold text-white'>MEGA Storage Up to <br /> 512GB SSD/16GB RAM</h2>
+            <h3 className='text-2xl font-medium my-6'>Flexiable and Support Expanded Storage</h3>
+            <p className='text-[18px] font-medium'>The up to 512GB SSD features a 16GB DDR4 high efficiency storage. Large materials, files, videos Areallowed you to store as much as you need. Completelybidding farewell to low-storage anxiety. Meanwhile, it supports expand the storage to dual storage. </p>
+        </div>
+        <div className="image">
+            <img src="https://d13pvy8xd75yde.cloudfront.net/global/tablets/megabook-k16s-amd/sec6_pc_1.png" alt="" />
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Specs3
